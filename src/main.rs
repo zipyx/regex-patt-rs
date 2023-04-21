d5feb7075d4650252b68681a7a228dc6419618bb
Setephano <setephano@gmail.com>
2023-04-21 15:24:38 +1200

    add: new version of package matching and ownership

--- src/main.rs
+++ src/main.rs
@@ -17,29 +17,37 @@ fn verify_username(username: &str) -> bool {
     
     // normalize the username
     let normalized = username.nfkd().nfkc().nfd().filter(|c| c.is_alphanumeric()).collect::<String>();
-    let re = Regex::new(r"^[a-zA-Z0-9_]+$").unwrap();
 
-    if re.is_match(normalized.to_string().as_str()) {
-        println!("Username is valid {} == {}", 
-            username, 
-            normalized
-        );
-        return true
-    } else {
-        println!("Username is invalid {}", username);
-        return false
+    // check if string is first accepted by first regex
+    let re = Regex::new(r"^[a-zA-Z0-9_]+$").unwrap();
+    if re.is_match(normalized.as_str()) {
+        // println!("Username {} normalized as best as possible, previously {}", normalized, username);
+        return regex_pattern_match(normalized.as_str())
     }
+    regex_pattern_match(normalized.as_str())
+}
+
+fn regex_pattern_match(username: &str) -> bool {
+    let regex_patterns = read_file("regex.txt");
+    for pattern in regex_patterns {
+        let re = Regex::new(pattern.as_str()).unwrap();
+        if re.is_match(username) {
+            println!("Username {} | doesn't meet requirements", username, );
+            return false;
+        }
+    }
+    true
 }
 
 fn read_file(file_name: &str) -> Vec<String> {
 
-    let mut passwords = Vec::new();
+    let mut list = Vec::new();
     let file = File::open(file_name).unwrap();
     let reader = BufReader::new(file);
     for line in reader.lines() {
-        passwords.push(line.unwrap());
+        list.push(line.unwrap());
     }
-    passwords
+    list
 }
 
 fn main() {
@@ -55,16 +63,34 @@ fn main() {
         "Äffinjsafdsajfsajf",
     ].iter().map(|&s| s.to_string()).collect::<Vec<String>>();
 
+    // verify usernames
+    let usernames = vec![
+        "4ss",
+        "d1ck",
+        "f4gg0t",
+        "B00bs",
+        "b00bs",
+        "test",
+        "ℌ",
+        "ℍ",
+        "ÅEΩLI",
+        "007",
+        "Äffin",
+        "Äffinjsafdsajfsajf",
+    ].iter().map(|&s| verify_username(s)).collect::<Vec<bool>>();
+
+    // verify_username(test);
+
     // check weak passwords
     let weak_passwords = read_file("weakpasswords.txt");
     for pass in weak_passwords {
-        verify_password(pass.as_str());
+        verify_username(pass.as_str());
     }
 
     // check breached passwords
     let breached_passwords = read_file("breachedpasswords.txt");
     for pass in breached_passwords {
-        verify_password(pass.as_str());
+        // verify_password(pass.as_str());
     }
     
 }
